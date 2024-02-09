1f812e61187aa20f04e3cdb15340b2e301149afa
ProDanil126 <danilfromusk@gmail.com>
2024-02-09 13:45:27 +0300

    Added cappuccino task with editing DB

--- main.py
+++ main.py
@@ -8,26 +8,55 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 class DBCoffee(QMainWindow):
     def __init__(self):
         super(DBCoffee, self).__init__()
-        uic.loadUi('main.ui', self)
+        self.modified = {}
+        self.id_row = None
+        uic.loadUi('addEditCoffeeForm.ui', self)
         self.con = sqlite3.connect('coffee.sqlite')
-        self.pushButton.clicked.connect(self.select_data)
+        self.updateButton.clicked.connect(self.select_data)
+        self.saveButton.clicked.connect(self.save_results)
+        self.tableWidget.itemChanged.connect(self.item_changed)
         self.textEdit.setPlainText('SELECT * FROM Coffee')
+        self.header = None
         self.select_data()
 
     def select_data(self):
         query = self.textEdit.toPlainText()
-        res = self.con.cursor().execute(query).fetchall()
-        header = query[7:query.index('FROM')].strip().split(', ')
-        print(header)
-        if header == ['*']:
-            header = ['ID', 'name', 'variety', 'roasting', 'type', 'description', 'price', 'size']
-        self.tableWidget.setColumnCount(len(header))
-        self.tableWidget.setHorizontalHeaderLabels(header)
-        self.tableWidget.setRowCount(0)
+        cur = self.con.cursor()
+        res = cur.execute(query).fetchall()
+        self.tableWidget.setRowCount(len(res))
+        if not res:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f'Количество строк по запросу: {len(res)}')
+        self.tableWidget.setColumnCount(len(res[0]))
+        self.header = [description[0] for description in cur.description]
+        self.tableWidget.setHorizontalHeaderLabels(self.header)
         for i, row in enumerate(res):
-            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        if 'ID' in self.header:
+            self.modified[self.header[item.column()]] = item.text()
+            self.id_row = self.tableWidget.item(item.row(), self.header.index('ID')).text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            query = 'UPDATE Coffee SET\n'
+            titles = ', '.join([f"{col}='{self.modified.get(col)}'"
+                               for col in self.modified.keys()])
+            query += titles + f"WHERE ID = {self.id_row}"
+            cur.execute(query)
+            self.con.commit()
+            self.modified.clear()
+            self.statusBar().showMessage(f'Изменения в {titles} c ID: {self.id_row} сохранены')
+        elif 'ID' not in self.header:
+            self.statusBar().showMessage('Для изменения добавте колонку с ID')
+        else:
+            self.statusBar().showMessage('Изменений не обнаружено')
 
     def closeEvent(self, event):
         self.con.close()
